9a240ca2dca0aa41770dff01a7278236f061fb06
Luisa Rojas <r.luisaf@gmail.com>
2020-11-04 22:07:46 -0500

    Initial commit - adding all files

---
+++ README.md
@@ -0,0 +1,72 @@
+# <p align="center">Centered Title</p>
+
+<p align="center">
+    <a href="#overview">Overview</a>
+    &nbsp; • &nbsp;
+    <a href="#technologies">Technologies</a>
+    &nbsp; • &nbsp;
+    <a href="#usage">Usage</a>
+    &nbsp; • &nbsp;
+    <a href="#changelog">Changelog</a>
+    &nbsp; • &nbsp;
+    <a href="#contributing">Contributing</a>
+    &nbsp; • &nbsp;
+    <a href="#license">License</a>
+</p>
+
+<p align="center">
+<img src="https://img.shields.io/github/languages/count/user/repo.svg?style=flat" alt="languages-badge"/>
+<img src="https://img.shields.io/github/license/user/repo.svg?style=flat" alt="license-badge"/>
+<img src="https://img.shields.io/github/repo-size/user/repo" alt="repo-size-badge">
+<img src="https://img.shields.io/github/v/tag/user/repo" alt="tag-badge">
+<img src="https://img.shields.io/github/last-commit/user/repo" alt="last-commit-badge">
+<img src="https://img.shields.io/github/issues-raw/user/repo" alt="open-issues-badge">
+</p>
+
+
+
+## Overview
+
+Proin vitae nisi aliquam, euismod velit quis, tristique nunc. Sed placerat odio ex, ac euismod dolor condimentum luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus at nunc ac nunc eleifend euismod. Integer maximus tincidunt magna, nec fermentum risus tincidunt finibus. Duis ut pharetra nisl. Vestibulum facilisis felis ut elit semper, sit amet vulputate odio imperdiet. Proin velit augue, scelerisque vel mollis sed, viverra quis ipsum. Proin ut dolor sed nibh interdum rhoncus. Mauris sit amet ipsum diam. Sed faucibus, dolor et lobortis facilisis, turpis ligula convallis dolor, sagittis tempus mi lacus at urna
+
+
+
+## Technologies
+
+Nam molestie a lacus sit amet dapibus. Integer auctor, nibh quis euismod fermentum, urna felis maximus odio, nec convallis sapien lectus ut nunc. Integer commodo velit risus. Nunc ac blandit justo. Nullam tellus metus, faucibus a feugiat eget, sodales id metus. Etiam sagittis ac tortor ac scelerisque. Integer a ex hendrerit, congue libero sit amet, suscipit arcu. Nunc eget lectus sed orci aliquet placerat. Quisque porta tristique tellus at eleifend.
+
+
+
+## Usage
+
+### Inputs
+
+Nunc a dui vel diam blandit porta quis sit amet turpis. Duis sed consectetur purus. Sed accumsan mauris gravida ipsum sollicitudin dignissim. Etiam tincidunt finibus leo non luctus. Vestibulum dignissim sapien ut pellentesque rhoncus. Aliquam hendrerit magna sed lacinia laoreet. Proin congue, arcu ac vestibulum tristique, ipsum urna pretium lectus, vel condimentum elit felis eget augue. Nunc condimentum blandit eleifend. Vestibulum nisi massa, dignissim quis lectus vel, euismod maximus ex. Suspendisse potenti. Nam sollicitudin sem accumsan gravida faucibus. Etiam ornare, enim sit amet rutrum viverra, ipsum dui egestas ex, in eleifend mi dui posuere libero. Nullam nec efficitur est.
+
+### Execution
+
+Etiam imperdiet, ex ac interdum consectetur, purus erat fringilla lacus, sed efficitur elit libero ut turpis. In fermentum libero sit amet quam tincidunt, eget dictum diam bibendum. Duis elementum mi orci. Nullam eleifend tellus ut arcu euismod, at efficitur lectus condimentum. Quisque sed purus in nunc consectetur tristique in eu quam. Integer pellentesque, velit in posuere fringilla, ipsum lacus ultrices mauris, nec varius lacus nisl sit amet nunc. Donec ut arcu massa. Cras tincidunt tortor eros, ac rutrum ex rhoncus at. Fusce ullamcorper, lorem sit amet rhoncus fringilla, sapien purus sollicitudin tortor, tristique ornare lorem ipsum sit amet nibh. Morbi diam nunc, condimentum vel placerat vel, tristique eu dolor. Vivamus est metus, lacinia ac nisi sit amet, placerat viverra neque. Aliquam massa tellus, varius ut erat quis, aliquam accumsan metus. Etiam pulvinar at ante id pretium. Sed rutrum libero at bibendum facilisis. Phasellus efficitur enim tincidunt libero volutpat, ac suscipit erat porttitor.
+
+
+
+## Changelog
+You can find the changelog for the project in: [CHANGELOG.md](link-to-changelog)
+
+
+
+## Contributing
+Vestibulum est eros, rhoncus nec vehicula eu, aliquam non elit. Aliquam erat volutpat. Vivamus lorem ligula, bibendum nec vulputate vitae, dictum semper felis. Maecenas quis ex tincidunt, pharetra neque vitae, tincidunt sapien. Duis porta tincidunt dignissim. Duis gravida, mi ac mollis ultricies, velit arcu convallis lorem, a faucibus eros ex at sem. Etiam laoreet dapibus dui vel tempor. Suspendisse potenti. Integer imperdiet lorem in neque euismod euismod. Aliquam eu nisi vel est ultricies egestas semper eu turpis. Quisque fermentum elit in leo tempor congue. Proin tristique ante sit amet erat facilisis viverra. Suspendisse accumsan, purus ac rutrum cursus, justo leo finibus urna, ac tempus turpis lorem in mauris. Pellentesque lacus nunc, tristique ut tortor nec, posuere pharetra erat. Suspendisse pulvinar commodo leo eu volutpat. Aliquam vel orci massa.
+
+
+
+## License
+
+This project was released under the [???? License](http://www.opensource.org/licenses/MIT).
+
+Copyright &copy; 2020 Luisa Rojas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ run.py
@@ -0,0 +1,242 @@
+import requests
+import time
+import json
+import random
+import smtplib
+import ssl
+import socket
+from datetime import datetime
+from bs4 import BeautifulSoup
+from pathlib import Path
+from email.message import EmailMessage
+from user_agents import USER_AGENTS
+
+testing_env = False
+data = None
+seconds_to_check = 900  # sleep for 15 minutes before trying again
+
+def load_prev_db():
+
+	global data
+	with open('db.json') as f:
+		data = json.load(f)
+
+def update_db():
+
+	global data
+	with open('db.json', 'w') as f:
+		json.dump(data, f)
+
+def check_changes():
+
+	now = datetime.now()
+	current_time = now.strftime('%b %d, %Y at %I:%M %p')
+
+	headers = {'User-Agent': random.choice(USER_AGENTS)}
+
+	url = 'https://abcnews.go.com/Politics/live-updates/2020-election-campaign-vote/?id=73960714'
+	page = requests.get(url, headers=headers)
+	soup = BeautifulSoup(page.content, 'lxml')
+
+	all_counts = soup.select('div.electionCount__voteCountContainer')
+	biden_count = int(all_counts[0].text)
+	dump_count = int(all_counts[1].text)
+	# print(biden_count, dump_count)
+
+	dem_house_count = int(all_counts[4].text)
+	gop_house_count = int(all_counts[5].text)
+	# print(dem_house_count, gop_house_count)
+
+	dem_senate_count = int(all_counts[2].text)
+	gop_senate_count = int(all_counts[3].text)
+	# print(dem_senate_count, gop_senate_count)
+
+	# Check if the numbers have changed
+	email_message = ''
+	html_email_message = """\
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<style>
+				@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+				body {
+				font-family: 'Roboto', sans-serif;
+				background-color: white;
+				}
+				ul {
+					font-size: 115%;
+					margin-top: 5px;
+				}
+				h1, p.to-win {
+					margin: 0;
+				}
+			</style>
+		</head>
+		<body bgcolor=”#ffffff”>
+	"""
+
+	# Keep track of if any categories have been updated
+	changed = False
+
+	# Check presidency
+	email_message += 'Presidency (270 to win)\n' + ("-" *10) + '\n'
+	html_email_message += '<h1>Presidency</h1><p class="to-win">270 to win</p><ul>'
+
+	# Biden
+	if biden_count != data['president']['Biden']:
+		changed = True
+
+		email_message += f'* Biden: {data["president"]["Biden"]} -> {biden_count}'
+		html_email_message += f'<li style="color: blue;"><b>Biden</b>: {data["president"]["Biden"]} &rarr; {biden_count}'
+
+		data["president"]["Biden"] = biden_count
+	else:
+		email_message += f'* Biden: No change ({biden_count})'
+		html_email_message += f'<li style="color: grey;"><b>Biden</b>: No change ({biden_count})'
+	if (biden_count >= 270):
+		email_message += ' - WIN! 🎉'
+		html_email_message += ' - WIN! 🎉'
+	email_message += '\n'
+	html_email_message += '</li>'
+
+	# Dump
+	if dump_count != data['president']['Trump']:
+		changed = True
+
+		email_message += f'* Trump: {data["president"]["Trump"]} -> {dump_count}'
+		html_email_message += f'<li style="color: red;"><b>Trump</b>: {data["president"]["Trump"]} &rarr; {dump_count}'
+		
+		data["president"]["Trump"] = dump_count
+	else:
+		email_message += f'* Trump: No change ({dump_count})'
+		html_email_message += f'<li style="color: grey;"><b>Trump</b>: No change ({dump_count})'
+	if (dump_count >= 270):
+		email_message += ' - win'
+		html_email_message += ' - win'
+	email_message += '\n'
+	html_email_message += '</li>'
+
+	html_email_message += '</ul>'
+
+	# Check house
+	email_message += '\nHouse (218 for majority)\n' + ("-" *10) + '\n'
+	html_email_message += '<h1>House</h1><p class="to-win">218 for majority</p><ul>'
+
+	if dem_house_count != data['house']['dem']:
+		changed = True
+
+		email_message += f'* Dem: {data["house"]["dem"]} -> {dem_house_count}'
+		html_email_message += f'<li style="color: blue;"><b>Dem</b>: {data["house"]["dem"]} &rarr; {dem_house_count}'
+
+		data["house"]["dem"] = dem_house_count
+	else:
+		email_message += f'* Dem: No change ({dem_house_count})'
+		html_email_message += f'<li style="color: grey;"><b>Dem</b>: No change ({dem_house_count})'
+	if (dem_house_count >= 218):
+			email_message += ' - MAJORITY!'
+			html_email_message += ' - MAJORITY! 🎉'
+	email_message += '\n'
+	html_email_message += '</li>'
+	
+	if gop_house_count != data['house']['gop']:
+		changed = True
+
+		email_message += f'* Gop: {data["house"]["gop"]} -> {gop_house_count}'
+		html_email_message += f'<li style="color: red;"><b>Gop</b>: {data["house"]["gop"]} &rarr; {gop_house_count}'
+
+		data["house"]["gop"] = gop_house_count
+	else:
+		email_message += f'* Gop: No change ({gop_house_count})'
+		html_email_message += f'<li style="color: grey;"><b>Gop</b>: No change ({gop_house_count})'
+	if (gop_house_count >= 218):
+			email_message += ' - majority'
+			html_email_message += ' - majority'
+	email_message += '\n'
+	html_email_message += '</li>'
+	html_email_message += '</ul>'
+
+	# Check senate
+	email_message += '\nSenate (51 for majority)\n' + ("-" *10) + '\n'
+	html_email_message += '<h1>Senate</h1><p class="to-win">51 for majority</p><ul>'
+
+	if dem_senate_count != data['senate']['dem']:
+		changed = True
+
+		email_message += f'* Dem: {data["senate"]["dem"]} -> {dem_senate_count}'
+		html_email_message += f'<li style="color: blue;"><b>Dem</b>: {data["senate"]["dem"]} &rarr; {dem_senate_count}'
+		
+		data["senate"]["dem"] = dem_senate_count
+	else:
+		email_message += f'* Dem: No change ({dem_senate_count})'
+		html_email_message += f'<li style="color: grey;"><b>Dem</b>: No change ({dem_senate_count})'
+	if (dem_senate_count >= 51):
+		email_message += ' - MAJORITY!'
+		html_email_message += ' - MAJORITY 🎉'	
+	email_message += '\n'
+	html_email_message += '</li>'
+
+	if gop_senate_count != data['senate']['gop']:
+		changed = True
+
+		email_message += f'* Gop: {data["senate"]["gop"]} -> {gop_senate_count}'
+		html_email_message += f'<li style="color: red;"><b>Gop</b>: {data["senate"]["gop"]} &rarr; {gop_senate_count}'
+		
+		data["senate"]["gop"] = gop_senate_count
+	else:
+		email_message += f'* Gop: No change ({gop_senate_count})'
+		html_email_message += f'<li style="color: grey;"><b>Gop</b>: No change ({gop_senate_count})'
+	if (gop_senate_count >= 51):
+		email_message += ' - majority'
+		html_email_message += ' - majority'
+	email_message += '\n'
+	html_email_message += '</li>'
+	html_email_message += '</ul>'
+
+	print(email_message)
+	html_email_message += f"""\
+	<p style="color: grey; margin-top: 25px;">Visit source <a target="_blank" href="{url}">here</a></p>
+	</body>
+	</html>
+	"""
+
+	if changed or testing_env:
+		try:
+			if Path('env.py').exists():
+				from env import ENV
+
+				receiver_emails_str = ', '.join(ENV['receiver']['emails'])
+				if testing_env: receiver_emails_str = ENV['receiver']['emails'][0]
+				print(f'There are changes.\nSending notification e-mail notification to: {receiver_emails_str}')
+				
+				port = 465  # For SSL
+				smtp_server = "smtp.gmail.com"
+				sender_email = ENV['sender']['email']
+				password = ENV['sender']['password']
+
+				message = EmailMessage()
+				message['Subject'] = '🆕 Election Update'
+				if testing_env: message['Subject'] += ' - Test'
+				message['From'] = sender_email
+				message['To'] = receiver_emails_str
+				message.set_content(email_message)
+				message.add_alternative(html_email_message, subtype='html')
+
+				context = ssl.create_default_context()
+				with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+					server.login(sender_email, password)
+					server.send_message(message)
+
+		except Exception as e: print('\n', e) # No biggie if the e-mail is not sent
+
+	else: print('--> No changes <--')
+
+	print(f'\nRunning every {int(seconds_to_check/60)} minute(s)')
+	print(f'Last checked: {current_time}', '\n')
+
+while True:
+
+	load_prev_db()
+	check_changes()
+	update_db()
+
+	time.sleep(seconds_to_check)
