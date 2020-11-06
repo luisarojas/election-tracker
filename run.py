87b81c919310db80e8005a22d61e24a46ee282c5
Luisa Rojas <r.luisaf@gmail.com>
2020-11-06 01:48:51 -0500

    Some refactoring work

--- run.py
+++ run.py
@@ -10,12 +10,11 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from pathlib import Path
 from email.message import EmailMessage
-from user_agents import USER_AGENTS
 
-testing_env = False
+testing_env = True
+seconds_to_check = 900  # sleep for 15 minutes before trying again
 
 data_filename = '.db.json'
-seconds_to_check = 900  # sleep for 15 minutes before trying again
 data = None
 
 def load_prev_db():
@@ -33,7 +32,16 @@ def check_changes():
 	now = datetime.now()
 	current_time = now.strftime('%b %d, %Y at %I:%M %p')
 
-	headers = {'User-Agent': random.choice(USER_AGENTS)}
+	user_agents = [
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+	]
+
+	headers = {'User-Agent': random.choice(user_agents)}
 
 	url = 'https://abcnews.go.com/Politics/live-updates/2020-election-campaign-vote/?id=73960714'
 	page = requests.get(url, headers=headers)
@@ -42,165 +50,125 @@ def check_changes():
 	all_counts = soup.select('div.electionCount__voteCountContainer')
 	biden_count = int(all_counts[0].text)
 	dump_count = int(all_counts[1].text)
-	# print(biden_count, dump_count)
 
 	dem_house_count = int(all_counts[4].text)
 	gop_house_count = int(all_counts[5].text)
-	# print(dem_house_count, gop_house_count)
 
 	dem_senate_count = int(all_counts[2].text)
 	gop_senate_count = int(all_counts[3].text)
-	# print(dem_senate_count, gop_senate_count)
 
-	# Check if the numbers have changed
-	email_message = ''
-	html_email_message = """\
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<style>
-				@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-				body {
-				font-family: 'Roboto', sans-serif;
-				background-color: white;
-				}
-				ul {
-					font-size: 115%;
-					margin-top: 5px;
-				}
-				h1, p.to-win {
-					margin: 0;
-				}
-			</style>
-		</head>
-		<body bgcolor=”#ffffff”>
-	"""
+	plain_text_message = ''
+	html_message =  ''
+	with open('.templates/template.txt') as file: plain_text_message = file.read()
+	with open('.templates/template.html') as file: html_message = file.read()
 
 	# Keep track of if any categories have been updated
-	changed = False
+	data_changed = False
+	description_message = ''
 
-	# Check presidency
-	email_message += 'Presidency (270 to win)\n' + ("-" *10) + '\n'
-	html_email_message += '<h1>Presidency</h1><p class="to-win">270 to win</p><ul>'
 
+	# ------------------------------  Check presidency ------------------------------
 	# Biden
 	if biden_count != data['president']['Biden']:
-		changed = True
-
-		email_message += f'* Biden: {data["president"]["Biden"]} -> {biden_count}'
-		html_email_message += f'<li style="color: blue;"><b>Biden</b>: {data["president"]["Biden"]} &rarr; {biden_count}'
-
+		data_changed = True
+		description_message = f'{data["president"]["Biden"]} &rarr; {biden_count}'
 		data["president"]["Biden"] = biden_count
+		html_message = html_message.replace('[biden-color]', 'dem-color') # blue
 	else:
-		email_message += f'* Biden: No change ({biden_count})'
-		html_email_message += f'<li style="color: grey;"><b>Biden</b>: No change ({biden_count})'
-	if (biden_count >= 270):
-		email_message += ' - WIN! 🎉'
-		html_email_message += ' - WIN! 🎉'
-	email_message += '\n'
-	html_email_message += '</li>'
+		description_message = f'No change ({biden_count})'
+		html_message = html_message.replace('[biden-color]', 'no-change-color') # grey
+	
+	if (biden_count >= 270): description_message += ' - WIN! 🎉'
+
+	html_message = html_message.replace('[biden-description]', description_message)
+	plain_text_message = plain_text_message.replace('[biden-description]', description_message)
 
 	# Dump
 	if dump_count != data['president']['Trump']:
-		changed = True
-
-		email_message += f'* Trump: {data["president"]["Trump"]} -> {dump_count}'
-		html_email_message += f'<li style="color: red;"><b>Trump</b>: {data["president"]["Trump"]} &rarr; {dump_count}'
-		
+		data_changed = True
+		description_message = f'{data["president"]["Trump"]} &rarr; {dump_count}'
+		html_message = html_message.replace('[dump-color]', 'gop-color') # red
 		data["president"]["Trump"] = dump_count
 	else:
-		email_message += f'* Trump: No change ({dump_count})'
-		html_email_message += f'<li style="color: grey;"><b>Trump</b>: No change ({dump_count})'
-	if (dump_count >= 270):
-		email_message += ' - win'
-		html_email_message += ' - win'
-	email_message += '\n'
-	html_email_message += '</li>'
+		description_message = f'No change ({dump_count})'
+		html_message = html_message.replace('[dump-color]', 'no-change-color') # grey
+	
+	if (dump_count >= 270): description_message += ' - win'
+	
+	html_message = html_message.replace('[dump-description]', description_message)
+	plain_text_message = plain_text_message.replace('[dump-description]', description_message)
 
-	html_email_message += '</ul>'
 
-	# Check house
-	email_message += '\nHouse (218 for majority)\n' + ("-" *10) + '\n'
-	html_email_message += '<h1>House</h1><p class="to-win">218 for majority</p><ul>'
 
+	# ------------------------------ Check house ------------------------------ 
+	# Dem
 	if dem_house_count != data['house']['dem']:
-		changed = True
-
-		email_message += f'* Dem: {data["house"]["dem"]} -> {dem_house_count}'
-		html_email_message += f'<li style="color: blue;"><b>Dem</b>: {data["house"]["dem"]} &rarr; {dem_house_count}'
-
+		data_changed = True
+		description_message = f'{data["house"]["dem"]} &rarr; {dem_house_count}'
+		html_message = html_message.replace('[dem-house-color]', 'dem-color') # blue
 		data["house"]["dem"] = dem_house_count
 	else:
-		email_message += f'* Dem: No change ({dem_house_count})'
-		html_email_message += f'<li style="color: grey;"><b>Dem</b>: No change ({dem_house_count})'
-	if (dem_house_count >= 218):
-			email_message += ' - MAJORITY!'
-			html_email_message += ' - MAJORITY! 🎉'
-	email_message += '\n'
-	html_email_message += '</li>'
+		description_message = f'No change ({dem_house_count})'
+		html_message = html_message.replace('[dem-house-color]', 'no-change-color') # grey
 	
+	if (dem_house_count >= 218): description_message += ' - MAJORITY! 🎉'
+	
+	html_message = html_message.replace('[dem-house-description]', description_message)
+	plain_text_message = plain_text_message.replace('[dem-house-description]', description_message)
+	
+	# Gop
 	if gop_house_count != data['house']['gop']:
-		changed = True
-
-		email_message += f'* Gop: {data["house"]["gop"]} -> {gop_house_count}'
-		html_email_message += f'<li style="color: red;"><b>Gop</b>: {data["house"]["gop"]} &rarr; {gop_house_count}'
-
+		data_changed = True
+		description_message = f'{data["house"]["gop"]} &rarr; {gop_house_count}'
+		html_message = html_message.replace('[gop-house-color]', 'gop-color') # red
 		data["house"]["gop"] = gop_house_count
 	else:
-		email_message += f'* Gop: No change ({gop_house_count})'
-		html_email_message += f'<li style="color: grey;"><b>Gop</b>: No change ({gop_house_count})'
-	if (gop_house_count >= 218):
-			email_message += ' - majority'
-			html_email_message += ' - majority'
-	email_message += '\n'
-	html_email_message += '</li>'
-	html_email_message += '</ul>'
+		description_message = f'No change ({gop_house_count})'
+		html_message = html_message.replace('[gop-house-color]', 'no-change-color') # grey
 
-	# Check senate
-	email_message += '\nSenate (51 for majority)\n' + ("-" *10) + '\n'
-	html_email_message += '<h1>Senate</h1><p class="to-win">51 for majority</p><ul>'
+	if (gop_house_count >= 218): description_message += ' - majority'
 
+	html_message = html_message.replace('[gop-house-description]', description_message)
+	plain_text_message = plain_text_message.replace('[gop-house-description]', description_message)
+
+
+
+	# ------------------------------ Check senate ------------------------------
+	# Dem
 	if dem_senate_count != data['senate']['dem']:
-		changed = True
-
-		email_message += f'* Dem: {data["senate"]["dem"]} -> {dem_senate_count}'
-		html_email_message += f'<li style="color: blue;"><b>Dem</b>: {data["senate"]["dem"]} &rarr; {dem_senate_count}'
-		
+		data_changed = True
+		description_message = f'{data["senate"]["dem"]} &rarr; {dem_senate_count}'
+		html_message = html_message.replace('[dem-senate-color]', 'dem-color') # blue
 		data["senate"]["dem"] = dem_senate_count
 	else:
-		email_message += f'* Dem: No change ({dem_senate_count})'
-		html_email_message += f'<li style="color: grey;"><b>Dem</b>: No change ({dem_senate_count})'
-	if (dem_senate_count >= 51):
-		email_message += ' - MAJORITY!'
-		html_email_message += ' - MAJORITY 🎉'	
-	email_message += '\n'
-	html_email_message += '</li>'
+		description_message = f'No change ({dem_senate_count})'
+		html_message = html_message.replace('[dem-senate-color]', 'no-change-color') # grey
+	
+	if (dem_senate_count >= 51): description_message += ' - MAJORITY 🎉'
 
+	html_message = html_message.replace('[dem-senate-description]', description_message)
+	plain_text_message = plain_text_message.replace('[dem-senate-description]', description_message)
+
+	# Gop
 	if gop_senate_count != data['senate']['gop']:
-		changed = True
-
-		email_message += f'* Gop: {data["senate"]["gop"]} -> {gop_senate_count}'
-		html_email_message += f'<li style="color: red;"><b>Gop</b>: {data["senate"]["gop"]} &rarr; {gop_senate_count}'
-		
+		data_changed = True
+		description_message = f'{data["senate"]["gop"]} &rarr; {gop_senate_count}'
+		html_message = html_message.replace('[gop-senate-color]', 'gop-color') # red
 		data["senate"]["gop"] = gop_senate_count
 	else:
-		email_message += f'* Gop: No change ({gop_senate_count})'
-		html_email_message += f'<li style="color: grey;"><b>Gop</b>: No change ({gop_senate_count})'
-	if (gop_senate_count >= 51):
-		email_message += ' - majority'
-		html_email_message += ' - majority'
-	email_message += '\n'
-	html_email_message += '</li>'
-	html_email_message += '</ul>'
+		description_message = f'No change ({gop_senate_count})'
+		html_message = html_message.replace('[gop-senate-color]', 'no-change-color') # grey
 
-	print(email_message)
-	html_email_message += f"""\
-	<p style="color: grey; margin-top: 25px;">Visit source <a target="_blank" href="{url}">here</a></p>
-	</body>
-	</html>
-	"""
+	if (gop_senate_count >= 51): description_message += ' - majority'
 
-	if changed or testing_env:
+	html_message = html_message.replace('[gop-senate-description]', description_message).replace('[url-source]', url)
+	plain_text_message = plain_text_message.replace('[gop-senate-description]', description_message).replace('&rarr;', '->')
+
+	# ------------------------------------------------------------------------------------------
+	
+	print(plain_text_message, '\n')
+	
+	if data_changed or testing_env:
 		if Path('env.py').exists():
 			from env import ENV
 			try:
@@ -224,8 +192,8 @@ def check_changes():
 				message['Subject'] = subject
 				message['From'] = sender_email
 				message['To'] = recipient_emails_str
-				message.set_content(email_message)
-				message.add_alternative(html_email_message, subtype='html')
+				message.set_content(plain_text_message)
+				message.add_alternative(html_message, subtype='html')
 
 				context = ssl.create_default_context()
 				with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
@@ -233,15 +201,14 @@ def check_changes():
 					server.send_message(message)
 			except Exception as e: print(e)
 
-	else: print('--> No changes <--')
+	else: print('*** No changes ***')
 
 	print(f'\nRunning every {int(seconds_to_check/60)} minute(s)')
 	print(f'Last checked: {current_time}', '\n')
 
 if __name__ == "__main__":
 
-	if testing_env:
-		print('\033[93m*** IN TESTING ENVIRONMENT ***\033[0m')
+	if testing_env: print('\033[93m*** IN TESTING ENVIRONMENT ***\033[0m')
 	
 	while True:
 
